661c7c38a958c2f29e9f0e8a9e52ee95062901fb
Aurélien Savart <aurelien.savart@knytify.com>
2024-05-28 14:15:11 +0200

    Commit modif connexion

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\User;
@@ -10,24 +9,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
-use App\Security\LoginFormAuthenticator;
+use Symfony\Bundle\SecurityBundle\Security;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, AuthenticationUtils $authenticationUtils, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, AuthenticationUtils $authenticationUtils, Security $security, UserRepository $userRepository): Response
     {
+        $error = null;
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $loginForm = $this->createForm(LoginFormType::class);
         $form->handleRequest($request);
         $loginForm->handleRequest($request);
 
+        // Initialiser les variables d'erreur et de dernier nom d'utilisateur
+
+        $lastUsername = '';
+
         if ($form->isSubmitted() && $form->isValid() && $request->request->has('register_submit')) {
-            // encode the plain password
+            // Encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -38,42 +43,34 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
+            // Do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_home');
         }
 
-        if ($loginForm->isSubmitted() && $loginForm->isValid() && $request->request->has('login_submit')) {
-                
-            $data = $form->getData();
+        if ($loginForm->isSubmitted() && $request->request->has('login_submit')) {
+            $email = $loginForm->get('username')->getData();
+            $password = $loginForm->get('password')->getData();
+            $user = $userRepository->findOneBy(['email' => $email]);
 
-            // récupérer l'utilisateur de la base de données
-            $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $data['username']]);
-
-            $requestData = [
-                '_username'=>$request->request->get('username'), 
-                '_password'=>$request->request->get('password') 
-            ];
-            
-
-            return $guardHandler->authenticateUserAndHandleSuccess(
-                   $user, // l'utilisateur que vous voulez connecter
-                   $requestData,
-                   $authenticator, // votre authentificateur
-                'main' // le nom du pare-feu dans security.yaml
-                );
-            /* return $this->forward('Symfony/Component/Security'); */
+            if ($user) {
+                if ($userPasswordHasher->isPasswordValid($user, $password)) {
+                    $security->login($user);
+                    return $this->redirectToRoute('app_home');
+                } else {
+                    $error = 'Invalid credentials.';
+                }
+            } 
+            else {
+                $error = 'Invalid credentials.';
+            }
         }
-
-           // get the login error if there is one
-           $error = $authenticationUtils->getLastAuthenticationError();
-
-           // last username entered by the user
-           $lastUsername = $authenticationUtils->getLastUsername();
-
+        // $error = $authenticationUtils->getLastAuthenticationError();
+        // $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form,
-            'loginForm' => $loginForm
+            'registrationForm' => $form->createView(),
+            'loginForm' => $loginForm->createView(),
+            'error' => $error,
         ]);
     }
 }
