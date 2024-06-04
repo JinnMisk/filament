96b3249daf710fe8ac5da875f5f1b0e2d9ab087d
JinnMisk <166838355+JinnMisk@users.noreply.github.com>
2024-06-04 11:54:40 +0200

    mise en forme du dashboard

--- src/Controller/MyBulbsController.php
+++ src/Controller/MyBulbsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BulbRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class MyBulbsController extends AbstractController
 {
     #[Route('/my/bulbs', name: 'app_my_bulbs')]
-    public function index(): Response
+    public function index(BulbRepository $repository): Response
     {
+        $bulbs = $repository -> findAll();
+
         return $this->render('my_bulbs/myBulbs.html.twig', [
             'controller_name' => 'MyBulbsController',
+            'bulbs' => $bulbs
         ]);
     }
 }
+
+   
