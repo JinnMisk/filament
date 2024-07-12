85b06d7256b96729f976fef539592db3ab144fdf
Oguz9355 <166838427+Oguz9355@users.noreply.github.com>
2024-07-12 12:25:16 +0200

    dernier push 12/07

--- src/Controller/ViewPlaceController.php
+++ src/Controller/ViewPlaceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PlaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,26 @@ use Symfony\Component\Routing\Attribute\Route;
 class ViewPlaceController extends AbstractController
 {
     #[Route('/view/place', name: 'app_view_place')]
-    public function index(): Response
+    public function index(PlaceRepository $placeRepository): Response
     {
-        return $this->render('view_place/viewPlace.html.twig', [
+        
+        $places = $placeRepository->findAll();
+
+        return $this->render('view_place/viewPlaces.html.twig', [
             'controller_name' => 'ViewPlaceController',
+            'places' => $places
         ]);
     }
-}
+
+    #[Route('/view/place/{id}', name: 'app_view_one_place')]
+    public function viewOnePlace(int $id, PlaceRepository $placeRepository): Response
+    {
+        
+        $place = $placeRepository->findOneBy(["id" => $id]);
+
+        return $this->render('view_place/viewPlace.html.twig', [
+            'controller_name' => 'ViewPlaceController',
+            'place' => $place
+        ]);
+    }
+}   
